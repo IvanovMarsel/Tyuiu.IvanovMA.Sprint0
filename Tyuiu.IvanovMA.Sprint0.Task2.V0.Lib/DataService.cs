d7fc4645447da0c36eaa5a3145ff8d390f770bc3
M <alwazalwaez@gmail.com>
2026-09-09 01:49:59 +0600

    Task2

---
+++ Tyuiu.IvanovMA.Sprint0.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Emit;
+
+namespace Tyuiu.IvanovMA.Sprint0.Task2.V0.Lib
+{
+    public class DataService
+    {
+        public static string GetMessage(string name)
+        {
+            return $"Привет, {name}";
+        }
+           
+    }
+}
---
+++ Tyuiu.IvanovMA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.IvanovMA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.IvanovMA.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вызов клааса DataService и метода GetMessage
+            //из библиотеки Tyuiu.IvanovMA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Марсель"));
+            Console.ReadKey();
+        }
+    }
+}
