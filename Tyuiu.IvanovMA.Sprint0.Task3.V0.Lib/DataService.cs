2b87165a199589ad45fe5f823156f80e2fa9d62c
M <alwazalwaez@gmail.com>
2026-09-10 19:03:08 +0600

    Task3

---
+++ Tyuiu.IvanovMA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.IvanovMA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        //Реализация методов Sum
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
